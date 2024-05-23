53ddfd178d48cb1d2863b525bc7aa13ba5ffd4fe
Bruno Campioni <bruno.olv.campioni@gmail.com>
2024-05-22 23:50:33 -0300

    commit da noite

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,23 +3,23 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#000';
+const tintColorDark = '#000';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#000',
     tint: tintColorLight,
-    icon: '#687076',
+    icon: '#000',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
+    text: '#000',
     background: '#151718',
     tint: tintColorDark,
-    icon: '#9BA1A6',
+    icon: '#000',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
